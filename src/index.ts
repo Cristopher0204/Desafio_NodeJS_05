c61beb79e5c1afd36294011a6a1706d06b3b5fdb
cristopher0204 <christophersilva954240@gmail.com>
2022-03-24 23:00:12 -0300

    foi usado o comando object excel csv

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { NOMEM } from "dns";
 
 dotenv.config();
 
@@ -41,3 +42,57 @@ app.listen(PORT, () => {
 	// R01, R02, R03, R04, R05
 	
 });
+class Aluno {
+	static forEach: any;
+	
+	static push() {
+		throw new Error("Method not implemented.");
+	}
+	nome: string;
+	idade: Number;
+	nota: Number;
+	
+	constructor(nome: string,idade: Number,nota: Number){
+	this.nome=nome;
+	this.idade=idade;
+	this.nota=nota;
+	}
+	}
+	const aluno = new Aluno('cris',21,5)
+	let alunos:Array<Aluno> = [];
+	
+	const readlinesync = require('readline-sync');
+	
+	let quantidadeDeAlunos = readlinesync.question('Quantos alunos são? \n')
+	
+	for (let index = 1; index <= quantidadeDeAlunos; index++) {
+		let nome = readlinesync.question(`nome do ${index} aluno: \n`)
+		let idade=parseInt(readlinesync.question(`idade do ${index} aluno: \n`))
+		let nota =parseInt(readlinesync.question(`Informe a nota do ${index} aluno: \n`))
+	
+		const aluno = new Aluno(nome,idade,nota)
+		
+	 alunos.push(aluno);
+	
+	}
+	 console.log(alunos);
+	 
+	 let somaDaNota = 0;
+		
+	 alunos.forEach(function(a: any , nota: number ){
+	 somaDaNota += a.nota
+	
+	 console.log(alunos)
+	 console.log(somaDaNota)
+	 });
+	 const ObjectsToCsv = require('objects-to-csv');
+
+	 (async () => {
+	   const csv = new ObjectsToCsv(alunos);
+	  
+	   
+	   await csv.toDisk('./test.csv');
+	  
+	   
+	   console.log(await csv.toString());
+	 })();
